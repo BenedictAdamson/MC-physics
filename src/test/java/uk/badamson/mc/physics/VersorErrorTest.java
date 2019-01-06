4ffe563306f58bad2156a1fe888381f3a85fdded
Benedict Adamson <badamson@spamcop.net>
2019-01-06 14:53:47 +0000

    Update reinstated old files.
    Add copyright headers.

--- src/test/java/uk/badamson/mc/physics/VersorErrorTest.java
+++ src/test/java/uk/badamson/mc/physics/VersorErrorTest.java
@@ -1,4 +1,21 @@
-package uk.badamson.mc.physics;
+package uk.badamson.mc.physics;/* 
+ * © Copyright Benedict Adamson 2018-19.
+ * 
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-physics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-physics.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.IsCloseTo.closeTo;
