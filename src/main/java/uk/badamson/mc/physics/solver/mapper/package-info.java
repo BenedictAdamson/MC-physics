4c5548fad4561c40b3b52a1cf12d1e8928922d9f
Benedict Adamson <badamson@spamcop.net>
2019-01-06 15:00:27 +0000

    Add package uk.badamson.mc.physics.solver.mapper

---
+++ src/main/java/uk/badamson/mc/physics/solver/mapper/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * <p>
+ * Classes for mapping from an object representation to (part of) a state-space
+ * representation, and vice versa.
+ * </p>
+ */
+package uk.badamson.mc.physics.solver.mapper;
+/*
+ * © Copyright Benedict Adamson 2018-19.
+ *
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MC-physics is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MC-physics. If not, see <https://www.gnu.org/licenses/>.
+ */
