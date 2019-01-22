1688c05ee101dcaad523136e7c34f8bc4e78f813
Benedict Adamson <badamson@spamcop.net>
2019-01-22 15:05:52 +0000

    Add interface ParticleTrajectory.

---
+++ src/main/java/uk/badamson/mc/physics/kinematics/ParticleTrajectory.java
@@ -0,0 +1,67 @@
+package uk.badamson.mc.physics.kinematics;
+
+import uk.badamson.mc.physics.TimeVaryingVector3;
+
+/*
+ * © Copyright Benedict Adamson 2018-19.
+ *
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-physics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-physics.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * <p>
+ * The continuous position of particle as a function of time.
+ * </p>
+ */
+public interface ParticleTrajectory {
+
+    /**
+     * <p>
+     * The acceleration vector of the particle as a function of time.
+     * </p>
+     * <ul>
+     * <li>Equal to the derivative of the {@linkplain #getVelocity() velocity
+     * vector}, and thus the second derivative of the {@linkplain #getPosition()
+     * position vector}. However, that equality may be subject to numerical
+     * errors.</li>
+     * </ul>
+     *
+     * @return the acceleration; not null.
+     */
+    public TimeVaryingVector3 getAcceleration();
+
+    /**
+     * <p>
+     * The position vector of the particle as a function of time.
+     * </p>
+     *
+     * @return the position; not null.
+     */
+    public TimeVaryingVector3 getPosition();
+
+    /**
+     * <p>
+     * The velocity vector of the particle as a function of time.
+     * </p>
+     * <ul>
+     * <li>Equal to the derivative of the {@linkplain #getPosition() position
+     * vector}. However, that equality may be subject to numerical errors.</li>
+     * </ul>
+     *
+     * @return the velocity; not null.
+     */
+    public TimeVaryingVector3 getVelocity();
+}
---
+++ src/test/java/uk/badamson/mc/physics/kinematics/ParticleTrajectoryTest.java
@@ -0,0 +1,36 @@
+package uk.badamson.mc.physics.kinematics;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/*
+ * © Copyright Benedict Adamson 2018-19.
+ *
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-physics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-physics.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * <p>
+ * Auxiliary test code for classes that implement the {@link ParticleTrajectory}
+ * interface.
+ */
+public class ParticleTrajectoryTest {
+
+    public static void assertInvariants(final ParticleTrajectory trajectory) {
+        assertNotNull(trajectory.getPosition(), "Not null, position");
+        assertNotNull(trajectory.getVelocity(), "Not null, velocity");
+        assertNotNull(trajectory.getAcceleration(), "Not null, acceleration");
+    }
+}
