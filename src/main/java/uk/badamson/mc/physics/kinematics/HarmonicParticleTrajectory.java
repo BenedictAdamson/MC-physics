cd760e495638f7df0e62e0ac09e8cfd6f2e034f7
Benedict Adamson <badamson@spamcop.net>
2019-01-23 22:47:34 +0000

    Add basic HarmonicParticleTrajectory

---
+++ src/main/java/uk/badamson/mc/physics/kinematics/HarmonicParticleTrajectory.java
@@ -0,0 +1,82 @@
+package uk.badamson.mc.physics.kinematics;
+/*
+ * © Copyright Benedict Adamson 2018-19.
+ *
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-physics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-physics.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import java.util.Objects;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import net.jcip.annotations.Immutable;
+import uk.badamson.mc.physics.HarmonicVector3;
+
+/**
+ * <p>
+ * The continuous position of particle as a function of time, allowing for the
+ * possibility of a constant acceleration and damped harmonic variation
+ * </p>
+ * <p>
+ * That also allows for exponentially decaying and growing variation as a
+ * special case of zero frequency of the sinusoidal variation.
+ * </p>
+ */
+@Immutable
+public final class HarmonicParticleTrajectory implements ParticleTrajectory {
+
+    private final HarmonicVector3 position;
+    private final HarmonicVector3 velocity;
+    private final HarmonicVector3 acceleration;
+
+    /**
+     * <p>
+     * Construct a trajectory from a position function.
+     * </p>
+     * <ul>
+     * <li>The {@linkplain #getPosition() position function} of this trajectory is
+     * the given position function.</li>
+     * </ul>
+     *
+     * @param position
+     *            The position vector of the particle as a function of time.
+     * @throws NullPointerException
+     *             If {@code position} is null.
+     */
+    public HarmonicParticleTrajectory(final HarmonicVector3 position) {
+        this.position = Objects.requireNonNull(position, "position");
+        velocity = position;// FIXME
+        acceleration = position;// FIXME
+    }
+
+    @Override
+    @NonNull
+    public final HarmonicVector3 getAcceleration() {
+        return acceleration;
+    }
+
+    @Override
+    @NonNull
+    public final HarmonicVector3 getPosition() {
+        return position;
+    }
+
+    @Override
+    @NonNull
+    public final HarmonicVector3 getVelocity() {
+        return velocity;
+    }
+
+}
---
+++ src/test/java/uk/badamson/mc/physics/kinematics/HarmonicParticleTrajectoryTest.java
@@ -0,0 +1,99 @@
+package uk.badamson.mc.physics.kinematics;
+/*
+ * © Copyright Benedict Adamson 2018-19.
+ *
+ * This file is part of MC-physics.
+ *
+ * MC-physics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MC-physics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MC-physics.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import uk.badamson.mc.ObjectTest;
+import uk.badamson.mc.math.ImmutableVector3;
+import uk.badamson.mc.physics.HarmonicVector3;
+
+/**
+ * <p>
+ * Unit tests and auxiliary test code for the class
+ * {@link HarmonicParticleTrajectory}.
+ * </p>
+ */
+public class HarmonicParticleTrajectoryTest {
+
+    @Nested
+    public class Constructor {
+
+        @Nested
+        public class Stationary {
+
+            @Test
+            public void a() {
+                final double we = 0.0;
+                final double wh = 0.0;
+                test(T_1, V_1, we, wh);
+            }
+
+            @Test
+            public void b() {
+                final double we = 5.0;
+                final double wh = 7.0;
+                test(T_2, V_2, we, wh);
+            }
+
+            private HarmonicParticleTrajectory test(final Duration t0, final ImmutableVector3 x, final double we,
+                    final double wh) {
+                final HarmonicVector3 position = new HarmonicVector3(t0, x, ImmutableVector3.ZERO,
+                        ImmutableVector3.ZERO, ImmutableVector3.ZERO, ImmutableVector3.ZERO, we, wh);
+
+                final HarmonicParticleTrajectory trajectory = Constructor.this.test(position);
+
+                return trajectory;
+            }
+        }// class
+
+        private HarmonicParticleTrajectory test(final HarmonicVector3 position) {
+            final HarmonicParticleTrajectory trajectory = new HarmonicParticleTrajectory(position);
+
+            assertInvariants(trajectory);
+            assertSame(position, trajectory.getPosition(),
+                    "The position function of this trajectory is the given position function.");
+            return trajectory;
+        }
+    }// class
+
+    private static final Duration T_1 = Duration.ofSeconds(1);
+
+    private static final Duration T_2 = Duration.ofSeconds(1);
+    private static final ImmutableVector3 V_1 = ImmutableVector3.I;
+    private static final ImmutableVector3 V_2 = ImmutableVector3.I.scale(2);
+
+    private static final ImmutableVector3 V_3 = ImmutableVector3.I.scale(3);
+
+    public static void assertInvariants(final HarmonicParticleTrajectory trajectory) {
+        ObjectTest.assertInvariants(trajectory);// inherited
+        ParticleTrajectoryTest.assertInvariants(trajectory);// inherited
+    }
+
+    public static void assertInvariants(final HarmonicParticleTrajectory trajectory1,
+            final HarmonicParticleTrajectory trajectory2) {
+        ObjectTest.assertInvariants(trajectory1, trajectory2);// inherited
+        ParticleTrajectoryTest.assertInvariants(trajectory1, trajectory2);// inherited
+    }
+}
